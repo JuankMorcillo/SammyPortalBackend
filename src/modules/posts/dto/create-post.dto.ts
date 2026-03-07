da93f9070e1842303df1f72da93257f7220dac10
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-07 13:32:09 -0500

    feature Se crea entity y dto para la creacion de post; se crea dto generico para manejo de respuestas

---
+++ src/modules/posts/dto/create-post.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
+export class CreatePostDto {
+
+    @ApiProperty({ description: 'ID del usuario autor del post', example: 1 })
+    @IsNumber()
+    authorUserId: number;
+
+    @ApiProperty({ description: 'Título del post', example: 'Mi primer post' })
+    @IsString()
+    title: string;
+
+    @ApiProperty({ description: 'Contenido del post', example: 'Este es el contenido de mi primer post' })
+    @IsString()
+    content: string;    
+}
---
+++ src/modules/posts/entities/post.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('posts')
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    authorUserId: number;
+
+    @Column()
+    title: string;
+
+    @Column({ nullable: true })
+    content: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+
+    @Column({ nullable: true, type: 'timestamp', onUpdate: 'CURRENT_TIMESTAMP' })
+    updated_at: Date;
+
+    @Column({ default: 1 })
+    status: number;
+}
