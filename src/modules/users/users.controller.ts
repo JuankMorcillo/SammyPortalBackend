31949a4e9477b08c9191b8b522973e289752eef0
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-07 13:09:55 -0500

    feature Se crea endpoint para la creacion de usuarios

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @ApiOperation({ summary: 'Create user' })
+  @Post()
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ description: 'The record has been successfully created.', status: 201 })
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+}
