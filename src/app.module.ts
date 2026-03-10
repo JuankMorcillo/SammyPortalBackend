b4c42f430e13ae21d89ebc1a4206ca7b239d3d33
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-10 11:14:04 -0500

    feature Se crea login dto y se agrega al proyecto configuracion para uso de autenticacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,20 @@ import { AppService } from './app.service';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import swaggerConfig from './config/swagger.config';
+import reqresConfig from './config/reqres.config';
 import { validateEnv } from './config/env.validation';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/guards/auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, databaseConfig, swaggerConfig],
+      load: [appConfig, databaseConfig, swaggerConfig, reqresConfig],
       validate: validateEnv,
     }),
     TypeOrmModule.forRootAsync({
@@ -25,13 +29,18 @@ import { PostsModule } from './modules/posts/posts.module';
       }),
     }),
     UsersModule,
-    PostsModule    
+    PostsModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [
-    AppService,    
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
   ],
 })
-export class AppModule { 
-  configure() {}
+export class AppModule {
+  configure() { }
 }
