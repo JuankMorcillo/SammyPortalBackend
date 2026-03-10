d2f1f72afae137d5d3c48a123b4ccfb27fe95ff8
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-10 11:14:52 -0500

    feature Se elimina endpoint que no se usara

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
 
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
@@ -15,13 +16,6 @@ export class UsersController {
   @ApiResponse({ description: 'The record has been successfully created.', status: 201 })
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
-  }
-
-  @ApiOperation({ summary: 'Check if user is registered' })
-  @Get(':email')  
-  @ApiResponse({ description: 'The user has been search.', status: 200 })
-  findByEmail(@Param('email') email: string): Promise<User> {
-    return this.usersService.isRegistered(email);
   }
 
 }
